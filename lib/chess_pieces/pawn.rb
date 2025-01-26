ff829802e3ae8068a11289ec106b8a6de410795e
dvgpro <dvg.prog@gmail.com>
2025-01-26 13:37:36 +0900

    feat: Add pawn's movement directions and symbol's

--- lib/chess_pieces/pawn.rb
+++ lib/chess_pieces/pawn.rb
@@ -1,5 +1,25 @@
+require-relative 'pieces'
+
 class Pawn < Pieces
+  attr_reader :movement_direction_set, :attack_direction_set, :piece_symbol
+
   def initialize
+    @movement_direction_set = [
+      [1,0],
+      [2,0]
+    ]
+
+    @attack_direction_set = [
+      [1,1],
+      [1,-1]
+    ]
+
+    @piece_symbol = assign_symbol_color
+
+  end
+
+  def assign_symbol_color
+    @self.team == "white" ? "\u2659" : "\u265f"
     
   end
 end
--- lib/chess_pieces/pieces.rb
+++ lib/chess_pieces/pieces.rb
@@ -1,9 +1,10 @@
 class Pieces
-  attr_reader :team, :captured, :initial_move
+  attr_reader :team, :captured, :initial_move, :location
 
   def initialize
     @team = ""
     @captured = False
     @initial_move = False
+    @location = [nil, nil]
   end
 end
